36457f06400236bda2fbee8f23e356e7827262c4
pietrolil <60861683+pietrolil@users.noreply.github.com>
2021-02-04 15:42:28 -0300

    Update Program.cs

--- Desafio2e3/Program.cs
+++ Desafio2e3/Program.cs
@@ -6,8 +6,6 @@ using VideoGame;
 class Program {
     static void Main(string[] args) {
 
-        Livro l1 = new Livro("Harry Potter", 40,50,"J.K Rowling","fantasia",300);
 
-        Console.WriteLine(l1.calcularImposto());
     }
 }
